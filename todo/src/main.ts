3d58487dde18efaab413bcf2466fe5ed8eea4a10
Weronika Materkowska <w.m.materkowska@gmail.com>
2023-09-12 20:23:37 +0200

    add cors

--- todo/src/main.ts
+++ todo/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 
 async function bootstrap() {
@@ -11,6 +12,11 @@ async function bootstrap() {
   const port: number | string = config.get('PORT');
 
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
+  app.enableCors({
+    origin: "http://localhost:4321/*",
+    methods: "GET, PATCH, POST, DELETE",
+  });
   await app.listen(port);
   console.log("App is running at port: " + port)
 }
